1aac27d7886e51535b3e5cc91be3b7baa85bb591
Elmahjouby Marouane <marouaneelmahjouby@gmail.com>
2020-06-02 23:39:21 +0100

    L'ajout du fonctionalité modification du mdp de proprietaire

--- src/Controller/ProprietaireController.php
+++ src/Controller/ProprietaireController.php
@@ -8,13 +8,15 @@ use App\Entity\Produit;
 use App\Form\ProduitType;
 use App\Form\UserPropType;
 use App\Entity\Proprietaire;
+use App\Form\PropChangePasswordType;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Validator\Constraints\UserPassword;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-
 
 /**
  * @Route("/proprietaire")
@@ -65,12 +67,38 @@ class ProprietaireController extends AbstractController
      /**
      * @Route("/compte", name="compte_proprietaire")
      */
-    public function compte(/*AuthenticationUtils $authenticationUtils*/)
+    public function compte(/*AuthenticationUtils $authenticationUtils*/Request $request,UserPasswordEncoderInterface $passwordEncoder)
     {
         //$repos = $this->getDoctrine()->getRepository(User::class)->findOneBy(['email' => $authenticationUtils->getLastUsername()]);
+
+        $em = $this->getDoctrine()->getManager();
+        $prop = $this->getUser();
+        $form = $this->createForm(PropChangePasswordType::class);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $oldpassword = $request->request->get('prop_change_password')['oldpassword'];
+            $newpassword = $request->request->get('prop_change_password')['confirmpassword']['first'];
+            // Si l'ancien mot de passe est bon
+         if($passwordEncoder->isPasswordValid($prop, $oldpassword)){
+            $newEncodedPassword = $passwordEncoder->encodePassword($prop, $newpassword);
+             $prop->setPassword($newEncodedPassword);
+            
+  
+              $em->flush();
+              $this->addFlash('notice', 'Votre mot de passe à bien été change !');
+  
+              return $this->redirectToRoute('compte_proprietaire');
+          }
+          else {
+              //$this->addFlash('danger', 'Ancien mot de passe incorrect !');
+  
+             $form->get('oldpassword')->addError(new FormError('Ancien mot de passe incorrect'));
+          }
+        }
         return $this->render('proprietaire/compte.html.twig',[
             'pagetitle'=>'Compte',
             'path'=>'compte_proprietaire',
+            'form'=>$form->createView()
             //'prenom'=>$repos->getPrenom()
         ]);
     }
