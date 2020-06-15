e8a4abec90d9588c112d4311d1f01b3dadb70ff1
Elmahjouby Marouane <marouaneelmahjouby@gmail.com>
2020-06-15 19:58:13 +0100

    Combobox Ville de prop

--- src/Form/PharmaChangeInfoType.php
+++ src/Form/PharmaChangeInfoType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PharmaChangeInfoType extends AbstractType
@@ -14,7 +15,31 @@ class PharmaChangeInfoType extends AbstractType
         $builder
             ->add('nom_pharmacie')
             ->add('adresse_pharmacie')
-            ->add('ville')
+            ->add('ville', ChoiceType::class, [
+                'choices'  => [
+                    'Casablanca' => 'Casablanca',
+                    'Fès' => 'Fès',
+                    'Salé' => 'Salé',
+                    'Tanger' => 'Tanger',
+                    'Marrakech' => 'Marrakech',
+                    'Meknès' => 'Meknès',
+                    'Rabat' => 'Rabat',
+                    'Oujda' => 'Oujda',
+                    'Kénitra' => 'Kénitra',
+                    'Agadir' => 'Agadir',
+                    'Tétouan' => 'Tétouan',
+                    'Témara' => 'Témara',
+                    'Safi' => 'Safi',
+                    'Mohammédia' => 'Mohammédia',
+                    'Khouribga' => 'Khouribga',
+                    'El Jadida' => 'El Jadida',
+                    'Béni Mellal' => 'Béni Mellal',
+                    'Nador' => 'Nador',
+                    'Taza' => 'Taza',
+                    'Khémisset' => 'Khémisset',
+                    'Autre...' => 'Autre',
+                ],
+            ])
             ->add('tel')
             ->add('Enregistrer', SubmitType::class, array (
                 'attr'=>array(
